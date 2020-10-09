62bb10419d286a3f3b95d957ee1bb68d37ae1625
Usama Subhani <reachusama@protonmail.com>
2020-10-09 23:23:13 +0500

    Add pin button

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -15,6 +15,16 @@ const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
             <div className="title">
                 <input type="text" value={title} readOnly={true} placeholder="Input title" />
             </div>
+
+
+            <div className="actions" onClick={event => event.stopPropagation()}>
+                {state !== 'TASK_ARCHIVED' && (
+                    // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                    <a onClick={() => onPinTask(id)}>
+                        <span className={`icon-star`} />
+                    </a>
+                )}
+            </div>
         </div>
     )
 }
