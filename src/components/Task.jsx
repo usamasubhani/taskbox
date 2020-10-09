c8e17ac04f7289beafda2759f81978b56ebafa64
Usama Subhani <reachusama@protonmail.com>
2020-10-09 23:21:52 +0500

    Add checkbox

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const Task = ({ task: { id, title, state }, onArchiveTask, onPinTask }) => {
     return (
-        <div>
-            <input type="text" value={title} readOnly={true} />
+        <div className="list-item">
+            <label className="checkbox">
+                <input
+                    type="checkbox"
+                    defaultChecked={state === 'TASK_ARCHIVED'}
+                    disabled={true}
+                    name="checked"
+                />
+                <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
+            </label>
+            <div className="title">
+                <input type="text" value={title} readOnly={true} placeholder="Input title" />
+            </div>
         </div>
     )
 }
